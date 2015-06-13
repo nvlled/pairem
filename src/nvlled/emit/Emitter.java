2e054dbba324104c1507e369f0fcdb8bdfb160c3
ron <nvlled@gmail.com>
2015-06-13 22:26:49 +0800

    Add iterator on emitter

--- src/nvlled/emit/Emitter.java
+++ src/nvlled/emit/Emitter.java
@@ -1,17 +1,18 @@
 package nvlled.emit;
 
-import java.util.concurrent.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Emitter<T> {
-    private List<T> frontEvents;
     private List<T> backEvents;
+    private List<T> frontEvents;
 
     private Set<Receiver<T>> anyListeners;
 
     public Emitter() {
         frontEvents = new LinkedList<T>();
         backEvents = new LinkedList<T>();
+        // TODO: use a set from concurrent one instread
         anyListeners = Collections.synchronizedSet(new HashSet<Receiver<T>>());
     }
 
@@ -49,10 +50,19 @@ public class Emitter<T> {
     }
 
     public T waitEvent() throws InterruptedException {
-        return waitEvent(new Predicate<T>() {
-            public boolean test(T e) {
-                return true;
-            }
+        return waitEvent(
+                new Predicate<T>() {
+                    public boolean test(T _) { return true; }
+                });
+    }
+
+    public Iterator<T> iterator(Predicate<T> pred) {
+        return new Iterator<T>(this, pred);
+    }
+
+    public Iterator<T> iterator() {
+        return new Iterator<T>(this, new Predicate<T>() {
+            public boolean test(T _) { return true; }
         });
     }
 
@@ -63,10 +73,50 @@ public class Emitter<T> {
         backEvents.clear();
 
         for (T e: frontEvents) {
-            System.out.println(e);
-
             for (Receiver<T> l: anyListeners)
                 l.receive(e);
         }
     }
+
+    public static class Iterator<T> implements Receiver<T>, java.util.Iterator<T> {
+        private Predicate<T> pred;
+        private Emitter<T> emitter;
+        private BlockingQueue<T> items;
+
+        private boolean closed = false;
+
+        private Iterator(Emitter<T> emitter, Predicate<T> pred) {
+            this.emitter = emitter;
+            this.pred = pred;
+            items = new LinkedBlockingQueue<T>();
+            emitter.listen(this);
+        }
+
+        @Override
+        public void receive(T e) {
+            items.add(e);
+        }
+
+        @Override
+        public boolean hasNext() { return !closed; }
+
+        @Override
+        public T next() {
+            try {
+                return items.take();
+            } catch (InterruptedException e) {
+            }
+            close();
+            return null;
+        }
+
+        @Override
+        public void remove() { /* noped */ }
+
+        public void close() {
+            emitter.unlisten(this);
+            closed = true;
+        }
+    }
+
 }
